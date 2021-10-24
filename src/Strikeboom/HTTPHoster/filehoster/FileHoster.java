34de3dc79a60f4e0a23e3f2e4cdf03cc55bc5d70
Shubh Makadia <60586926+IStrikeboomI@users.noreply.github.com>
2021-10-23 20:54:55 -0500

    added comments

--- src/Strikeboom/HTTPHoster/filehoster/FileHoster.java
+++ src/Strikeboom/HTTPHoster/filehoster/FileHoster.java
@@ -14,10 +14,13 @@ import java.util.List;
 public class FileHoster {
     HttpServer server;
     int port;
+    //the user's ip address
     public static String ip;
+    //all the urls of the files
     List<String> fileURLs = new ArrayList<>();
     public FileHoster(int port) {
         try {
+            //these 3 lines are to get the users ip through the site
             BufferedReader reader = new BufferedReader(new InputStreamReader(new URL("http://bot.whatismyipaddress.com/").openStream()));
             ip = reader.readLine().trim();
             reader.close();
@@ -30,14 +33,19 @@ public class FileHoster {
         }
     }
     public void host(File file, String filePath) {
+        //add file with path
         fileURLs.add("http://" + ip + ":" + port + filePath);
+        //create a new context for the filepath
         server.createContext(filePath,httpExchange -> {
+            //give the headers only if there is a header
             if (MimeHeaders.hasExtension(filePath)) {
                 if (MimeHeaders.hasMimeType(MimeHeaders.getExtension(filePath))) {
                     httpExchange.getResponseHeaders().add("Content-Type",MimeHeaders.getMimeTypeFromExtension(MimeHeaders.getExtension(filePath)));
                 }
             }
+            //convert file to byte array
             byte[] bytes = Files.readAllBytes(file.toPath());
+            //send over bytes
             httpExchange.sendResponseHeaders(200,bytes.length);
             OutputStream os = httpExchange.getResponseBody();
             os.write(bytes);
@@ -46,6 +54,7 @@ public class FileHoster {
         });
     }
     public void start() {
+        //only start if not started yet
         if (!Main.isRunning) {
             server.start();
             Main.isRunning = true;
